0b11845476b8c57960489af754bccb44ec2014fd
Yegor Ivashchenko <yivashch@softserveinc.com>
2020-05-04 17:15:21 +0300

    Mouse wheel scrolling doesn't work correctly

---
+++ content.js
@@ -0,0 +1,70 @@
+const DIVIDING_LINE_HEIGHT = 1
+const DEFAULT_DELTA = 1
+function main() {
+    console.log("Initializing extension")
+    let canvas = document.createElement("canvas")
+    canvas.id = "mozreader"
+    let viewportHeight = window.innerHeight - 20
+    let CANVAS_WIDTH = window.innerWidth - 30
+    canvas.width = CANVAS_WIDTH
+    canvas.height = viewportHeight
+    canvas.setAttribute("style", "border: 1px solid blue;")
+    document.body.appendChild(canvas)
+    let ctx = canvas.getContext('2d', {alpha: false})
+    ctx.imageSmoothingEnabled = true
+    ctx.imageSmoothingQuality = "high"
+    ctx.drawWindow(window, 0, 0, CANVAS_WIDTH, viewportHeight, 'rgb(255,255,255)');
+    // document will be drawn starting from this y position
+    let documentOffset = viewportHeight - DEFAULT_DELTA
+    // where in the viewport new content will be drawn
+    let viewportOffset = viewportHeight - DEFAULT_DELTA
+    function scroll(delta = DEFAULT_DELTA) {
+        ctx.save()
+        // start drawing next viewport including last pixels of current
+        // if (viewportOffset % viewportHeight === 0) {
+        //         documentOffset -= 30
+        // }
+        let newDocumentOffset = documentOffset + delta
+        let newViewportOffset = (viewportHeight + viewportOffset + delta) % viewportHeight
+        ctx.translate(0, newViewportOffset)
+        ctx.drawWindow(window, 0, newDocumentOffset, CANVAS_WIDTH, delta, 'rgb(255,255,255)');
+        ctx.fillRect(0, delta, CANVAS_WIDTH, DIVIDING_LINE_HEIGHT)
+        documentOffset = newDocumentOffset
+        viewportOffset = newViewportOffset
+        ctx.restore()
+    }
+    let intervalId = setInterval(
+        scroll,
+        100
+    )
+    canvas.addEventListener("click", event => {
+        if (intervalId) {
+            intervalId = clearInterval(intervalId)
+        }
+        else {
+            intervalId = setInterval(
+                scroll,
+                100
+            )
+        }
+    })
+    document.addEventListener("wheel", event => {
+        if (event.target === canvas){
+            console.log(event.deltaY)
+
+            // scroll(Math.min(event.deltaY, 30))
+            if (event.deltaY > 0) {
+                scroll(event.deltaY)
+            }
+        }
+    })
+    console.log("Initializing extension done")
+}
+console.log("About to set timeout on main...")
+setTimeout(main, 2000)
+
+// document.addEventListener('readystatechange', event => {
+//     if (event.target.readyState === "complete") {
+//         main()
+//     }
+// })
